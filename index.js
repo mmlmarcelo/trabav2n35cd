f2c66cd9e60e50f7c7021cca1ce0419a3f486946
mmlmarcelo <mml.marcelo@hotmail.com>
2025-04-22 18:13:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+
+//criando a rota inicial
+app.get("/", function(req,res){
+    res.send("<h1>Site para avaliação av2</h1>");
+})
+
+//rota do cadastro de produtos
+app.get("/presentes", function(req,res){
+    res.send("<h1>lista de presentes</h1>");
+})
+
+//rota com parametro 
+app.get("/consulta/:parametro", function(req,res){
+    //req --> dados enviados pelo cliente
+    //res --> resposta enviada pelo servidor de volta ao cliente
+    res.send("retorno consulta:" + req.params.parametro);
+})
+
+
+app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 4000
+    if (erro){
+        console.log("Erro ao Iniciar.");
+    }else{
+        console.log("Servidor Iniciado.");
+    }
+})
+
